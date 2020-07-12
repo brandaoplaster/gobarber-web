5f30c1939506fa6cbc0a50511347f1c435ee9003
brandaoplaster <brandaoplaster@gmail.com>
2020-07-12 18:40:09 -0300

    Remove toast message

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
-import { ToastMessage } from '../../hooks/toast';
+import { ToastMessage, useToast } from '../../hooks/toast';
 import { Conatainer, Toast } from './styles';
 
 interface ToastContainerProps {
@@ -8,6 +8,8 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const { removeToast } = useToast();
+
   return (
     <Conatainer>
       {messages.map(message => (
@@ -20,7 +22,7 @@ const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
           <div>
             <strong>{message.title}</strong>
             {message.description && <p>{message.description}</p>}
-            <button type="button">
+            <button onClick={() => removeToast(message.id)} type="button">
               <FiXCircle size={18} />
             </button>
           </div>
